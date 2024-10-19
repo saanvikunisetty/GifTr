35a426186385f8339f3f96f0d574100d65bcec11
TejasRaghuram <tejas.raghuram101@gmail.com>
2024-10-18 23:28:35 -0700

    Created Navbar

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,9 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Logo from "./../images/Logo.png";
+import "./../styles/Layout.css";
 
 function Layout() {
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
     return (
         <>
-            <p>This is a Header.</p>
+            { 
+                (location.pathname.substring(1).toUpperCase() == "MARKET" || 
+                location.pathname.substring(1).toUpperCase() == "WALLET") &&
+                <>
+                    <div id="navbar">
+                        <img id="logo" src={Logo} alt=""/>
+                        <button className="link" onClick={() => {navigate("/market")}}>Market</button>
+                        <button className="link" onClick={() => {navigate("/wallet")}}>Wallet</button>
+                        <button id="logout" onClick={() => {navigate("/login")}}>Log Out</button>
+                    </div>
+                    <div id="navbar-space" />
+                </>
+            }
             <Outlet />
             <p>This is a Footer.</p>
         </>
