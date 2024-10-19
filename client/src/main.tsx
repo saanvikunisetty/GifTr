858a851f482ee25b6b5987ac630122b435722f72
TejasRaghuram <tejas.raghuram101@gmail.com>
2024-10-18 23:50:58 -0700

    Created Error Page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,6 @@ import "@radix-ui/themes/styles.css";
 
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Theme } from "@radix-ui/themes";
 import { router } from "./pages/Index.tsx"
 import { networkConfig } from "./networkConfig.ts";
 import { RouterProvider } from "react-router-dom";
@@ -14,7 +13,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="light">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
           <WalletProvider autoConnect>
@@ -22,6 +20,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </WalletProvider>
         </SuiClientProvider>
       </QueryClientProvider>
-    </Theme>
   </React.StrictMode>,
 );
--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,7 +1,10 @@
+import './../styles/Error.css';
+
 function Error() {
     return (
         <>
-            <p>This is the Error Page.</p>
+            <h1 id="error-title">ERROR 404</h1>
+            <h2 id="error-message">Page Not Found</h2>
         </>
     );
 }
